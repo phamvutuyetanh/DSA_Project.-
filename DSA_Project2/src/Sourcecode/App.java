da47cb5903facdbf6d64a79702b287f69736e33f
phamvutuyetanh <phamvutuyetanh@gmail.com>
2023-12-11 02:03:19 +0700

    Add base code and change selection sort

---
+++ DSA_Project2/src/Sourcecode/App.java
@@ -0,0 +1,188 @@
+package Sourcecode;
+import java.awt.event.*;
+import java.awt.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class App extends JFrame implements ActionListener {
+
+    // Instantiate Objects
+
+    // Random stuff
+     JComboBox algosDropdown;
+     JLabel lblTtl;
+     JLabel name;
+     String selectedAlgo = "";
+     JPanel panelUpper = new JPanel();
+    CreateArray newArray = new CreateArray();
+    ArrayList<Integer> array = newArray.createArray();//create array list with random position
+    Draw draw = new Draw(array);
+
+    // Buttons
+    JButton start;
+    JButton reset;
+
+    // Instantiate Algorithms
+    BubbleSort bubble = new BubbleSort();
+    SelectionSort selection = new SelectionSort(array);
+    InsertionSort insertion = new InsertionSort();
+    QuickSort quick = new QuickSort();
+
+    // Runtime, No. Comparisons, Array Accesses
+    // Objects
+    JLabel runtimeLabel;
+    JLabel comparisonsLabel;
+    JLabel arrayAccessesLabel;
+
+    // Global variables
+    int noComparisons = 0;
+    int noArrAccess = 0;
+
+    // Bool value for reset check
+    boolean needReset = false;
+
+    public App(){
+        // Instantiate stuff
+        this.setTitle("Sorting Algorithms Visualizer");
+        this.setSize(new Dimension(1500, 1000));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//chua chinh
+        // Title
+        lblTtl = new JLabel("Sorting Algorithms Visualizer");
+        lblTtl.setBounds(400,10,1000,100);
+        lblTtl.setFont(new Font("Niagara Solid", Font.BOLD, 80));
+        lblTtl.setForeground(Color.white);
+        panelUpper.add(lblTtl);
+
+        // Add Name
+        name = new JLabel("DSA-Project");
+        name.setBounds(650,120,206,40);
+        name.setFont(new Font("Niagara Solid", Font.BOLD, 50));
+        name.setForeground(Color.white);
+        panelUpper.add(name);
+
+        // Runtime, Number Comparisons, Number Array Accesses
+        // Runtime label
+        runtimeLabel = new JLabel("Runtime: null");
+        runtimeLabel.setBounds(20,180,250,50);
+        runtimeLabel.setFont(new Font("Niagara Solid", Font.BOLD, 28));
+        runtimeLabel.setForeground(Color.white);
+        panelUpper.add(runtimeLabel);
+
+        // No. Comparisons Label
+        comparisonsLabel = new JLabel("No. Comparisons: " + noComparisons);
+        comparisonsLabel.setBounds(20,250, 250,50);
+        comparisonsLabel.setFont(new Font("Niagara Solid", Font.BOLD, 28));
+        comparisonsLabel.setForeground(Color.white);
+        panelUpper.add(comparisonsLabel);
+
+        // No. Array Access Label
+        arrayAccessesLabel = new JLabel("No. Array Access: " + noArrAccess);
+        arrayAccessesLabel.setBounds(20,330,250,50);
+        arrayAccessesLabel.setFont(new Font("Niagara Solid", Font.BOLD, 28));
+        arrayAccessesLabel.setForeground(Color.white);
+        panelUpper.add(arrayAccessesLabel);
+      //sua
+        // Combo Box
+        String[] algorithms = {"Select Algorithm", "Bubble Sort", "Selection Sort", "Insertion Sort", "Quick Sort"};
+        algosDropdown = new JComboBox(algorithms);
+        algosDropdown.setBounds(1200,180,200,50);
+        algosDropdown.addActionListener(this);
+        panelUpper.add(algosDropdown);
+
+        // Buttons
+        // Start Button
+        start = new JButton("Start Visualization");
+        start.setBounds(1200, 250,200,50);//700, 30, 140,30
+        start.addActionListener(this);
+        panelUpper.add(start);
+
+        // Reset Button
+        reset = new JButton("Reset");
+        reset.setBounds(1200, 330, 200, 50);//700, 60, 140, 30. x,y,rong,dai
+        reset.addActionListener(this);
+        panelUpper.add(reset);
+
+        // Align two panels
+        panelUpper.setBounds(0,0,2000,500);///870,100
+        draw.setBounds(0,450,2000,800);//870,522
+
+        // Set background colours
+        panelUpper.setBackground(Color.black);
+        draw.setBackground(Color.black);
+
+        // Add panels
+        panelUpper.setLayout(new BorderLayout());
+        this.add(draw);
+        this.add(panelUpper);
+        this.validate();
+        this.setVisible(true);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Check status before pressing start button
+        if (e.getSource()==start & algosDropdown.getSelectedItem() != "Select Algorithm" & !needReset) {
+            if (selectedAlgo.equals("Bubble")) {
+                System.out.println("A");
+                try {
+                    bubble.executeBubbleSort(array, draw, this);
+                    System.out.println("B");
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                    System.out.println("C");
+                }
+
+            } else if (selectedAlgo == "Selection") {
+                try {
+                    selection.executeSelectionSort( draw, this);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+
+            } else if (selectedAlgo == "Insertion") {
+                try {
+                    insertion.executeInsertionSort(array, draw, this);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+
+            } else if (selectedAlgo == "Quick") {
+                try {
+                    quick.executeQuickSort(array, draw, this);
+                } catch (InterruptedException interruptedException) {
+                    interruptedException.printStackTrace();
+                }
+            }
+        }
+            // Reset button status
+            if (e.getSource()==reset) {
+                array = newArray.createArray();
+                draw.updateArray(array);
+                draw.repaint();
+                needReset = false;
+
+            }
+            // Combo box status
+        if (e.getSource()==algosDropdown) {
+            System.out.println(algosDropdown.getSelectedItem());
+            if (algosDropdown.getSelectedItem() == "Bubble Sort") {
+                selectedAlgo = "Bubble";
+                runtimeLabel.setText("Runtime: O(N^2)");
+
+            } else if (algosDropdown.getSelectedItem() == "Selection Sort") {
+                selectedAlgo = "Selection";
+                runtimeLabel.setText("Runtime: O(N^2)");
+
+            } else if (algosDropdown.getSelectedItem() == "Insertion Sort") {
+                selectedAlgo = "Insertion";
+                runtimeLabel.setText("Runtime: O(N^2)");
+
+            } else if (algosDropdown.getSelectedItem() == "Quick Sort") {
+                selectedAlgo = "Quick";
+                runtimeLabel.setText("Runtime: Nlog(N)");
+            }
+        }
+
+    }
+}
---
+++ DSA_Project2/src/Sourcecode/SelectionSort.java
@@ -0,0 +1,49 @@
+package Sourcecode;
+
+import javax.swing.*;
+
+//import java.awt.Color;
+import java.util.*;
+
+public class SelectionSort extends JPanel {
+	 private ArrayList<Integer> nums;                 // ref to array a
+	   private int nElems;               // number of data items
+	   public static int nCompare;
+	  
+	//--------------------------------------------------------------
+	   public SelectionSort (ArrayList<Integer> array)          // constructor
+	      {
+	      nums = array;                 // create the array
+	      nElems = array.size();                        // no items yet
+	      nCompare =0;
+	    
+	      }
+
+    public void executeSelectionSort(Draw draw, App app) throws InterruptedException {
+        int out, in, min; 
+	      for(out=0; out<nElems - 1; out++)   // outer loop
+	         {
+	         min = out;                     // minimum
+	         
+	         for(in=out+1; in<nElems; in++) { // inner loop
+	        	 nCompare++;
+	            if(nums.get(in) < nums.get(min) )         // if min greater,  
+	            	min = in;   }            // we have a new min
+	         swap(out, min);                // swap them
+	         Thread.sleep(100);
+	            draw.removeAll();
+	            draw.updateArray(nums);
+	            draw.revalidate();
+	            draw.paintImmediately(0,30,2000,1000);//870,532
+	         } 
+        app.needReset = true;
+    }
+    
+    private void swap(int one, int two)
+    {
+    int temp = nums.get(one);
+    int temp2 =  nums.get(two);
+    nums.set(one, temp2);
+    nums.set(two, temp);
+    }
+}
