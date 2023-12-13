96fe2b09eb54954c434550a7ea73814073bb684a
CeciliaDinh <dhang2003@gmail.com>
2023-12-13 18:26:40 +0700

    modify BubbleSort

--- DSA_Project2/src/SortingAlgorithm/BubbleSort.java
+++ DSA_Project2/src/SortingAlgorithm/BubbleSort.java
@@ -7,7 +7,7 @@ import Main.App;
 import Main.Draw;
 
 public class BubbleSort extends BaseSort{
-  // private boolean swapped = true;
+   private boolean swapped = true;
 	public BubbleSort (ArrayList<Integer> array)          // constructor
     {
 		super(array);
@@ -16,48 +16,29 @@ public class BubbleSort extends BaseSort{
 
 	@Override
 	public void excutesort(Draw draw, App app) throws InterruptedException {
-		int out,in;
+		int out,in, nElems;
 		int nSwaps=0;
-		for(out = nElems-1;out>1;out--){
-			for(in=0;in<out;in++){
-				if(nums.get(in)>nums.get(in+1)){
-					swap(in,in+1);
-					nSwaps++;
-					
-				}
-				Thread.sleep(25);
-					draw.removeAll();
-					draw.updateArray(nums);
-					draw.revalidate();
-					draw.paintImmediately(0,30,2000,1000);//870,532
-					System.out.println(nums);
-			}
-		}
-		//    BbSort(nums,draw);
-	    //     swapped = true;
-	      app.setneedReset(true);
-      
+		nElems= nums.size();
+		for(out=nElems-1;out>1;out--){
+	        for(in=0;in<out;in++){
+	            if(nums.get(in)>nums.get(in+1)){
+				    swap(in,in+1);	
+				    nSwaps++;
+				    Thread.sleep(25);
+				    draw.removeAll();
+				    draw.updateArray(nums);
+				    draw.revalidate();
+				    draw.paintImmediately(0,30,2000,1000);//870,532
+				    System.out.println(nums);
+	    
 
 	}
-	// private void BbSort(ArrayList<Integer> arr, Draw draw) throws InterruptedException {
-	// 	int out,in, nElems;
-	// 	int nSwaps=0;
-	// 	nElems= arr.size();
-	// 	for(out=nElems-1;out>1;out--){
-	// 		for(in=0;in<out;in++){
-	// 			if(arr.get(in)>arr.get(in+1)){
-	// 				swap(arr.get(in),arr.get(in+1));
-	// 				nSwaps++;
-	// 				Thread.sleep(25);
-	// 				draw.removeAll();
-	// 				draw.updateArray(nums);
-	// 				draw.revalidate();
-	// 				draw.paintImmediately(0,30,2000,1000);//870,532
-	// 				System.out.println(nums);
-	// 			}
-	// 		}
-	// 	}
-
-
-	// }
 }
+		}
+	}
+}
+	
+	 
+
+
+	 
