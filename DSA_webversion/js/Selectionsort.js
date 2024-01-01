2db77381fa801ab023a7f81807aad362894264da
phamvutuyetanh <phamvutuyetanh@gmail.com>
2024-01-02 00:40:06 +0700

    Add the selection sort algorithm. This extends Basesort

---
+++ DSA_webversion/js/Selectionsort.js
@@ -0,0 +1,35 @@
+class Selectionsort extends Basesort{
+    constructor(namebutton1, namebutton2, nameElement1){
+        super(namebutton1, namebutton2, nameElement1);
+        this.swapselect = 0;
+        this.compare = 0;
+    }
+    sortt(array){
+        var moves_select = [];
+        // i = out 
+        for (let i = 0; i < array.length - 1; i++) {
+            let minIndex = i;
+    //j = in
+            for (let j = i + 1; j < array.length; j++) {
+
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+    
+                moves_select.push({ indices: [i, j], swapp: false,  swapnumber: this.swapselect, ncompare: ++ this.compare });
+            }
+            //console.log(array_selection[minIndex]);
+    
+            if (minIndex !== i) {
+                [array[i], array[minIndex]] = [array[minIndex], array[i]];
+                moves_select.push({ indices: [i, minIndex], swapp: true,  swapnumber: ++this.swapselect, ncompare: this.compare});
+            } 
+            else {
+                moves_select.push({ indices: [i, minIndex], swapp: false,  swapnumber: this.swapselect, ncompare: this.compare });
+            }
+        }
+        this.swapselect = 0;
+        this.compare = 0;
+        return moves_select;
+}
+}
---
+++ DSA_webversion/js/scriptmain.js
@@ -0,0 +1,85 @@
+//Bubble sort
+// const canvasB = new Canvas('Canvas');
+//   const bubblesort = new Bubblesort("#select1A","#select1B","Big0Bubble" );
+
+//   function sortB(){
+//     canvasB.sort();
+//   }
+//   function createB(){
+//     bubblesort.create();
+//   }
+//   canvasB.addElement(bubblesort);
+// //   canvas.create();
+// canvasB.draw(); 
+//  bubblesort.create();
+
+
+// Selectionsort
+
+ const canvasS = new Canvas('myCanvasSelection');
+ const selection = new Selectionsort("#select2A","#select2B", "Big0Select");
+ function sortS(){
+   canvasS.sort();
+ }
+ function createS(){
+    selection.create();
+  }
+ canvasS.addElement(selection);
+canvasS.draw(); 
+selection.create();
+
+//Insertion
+
+// const canvasI = new Canvas('myCanvasInsertion');
+// const insertion = new Insertionsort("#select3A","#select3B", "Big0Insert");
+// function sortI(){
+//   canvasI.sort();
+// }
+// function createI(){
+//     insertion.create();
+//   }
+// canvasI.addElement(insertion);
+// canvasI.draw(); 
+// insertion.create();
+
+
+// Quick Sort  
+// const canvasQ = new Canvas('myCanvasQuick');
+// const quick = new QuickSort("#select4A", "#select4B","Big0Quick");
+// function sortQ(){
+//   canvasQ.sort();
+// }
+// function createQ(){
+//     quick.create();
+//   }
+// canvasQ.addElement(quick);
+// canvasQ.draw(); 
+// quick.create();
+
+
+// Merge sort   myCanvasMerge
+// const canvasM = new Canvas('myCanvasMerge');
+// const merge = new Mergesort("#select5A","#select5B", "Big0Merge");
+// function sortM(){
+//   canvasM.sort();
+// }
+// function createM(){
+//     merge.create();
+//   }
+// canvasM.addElement(merge);
+// canvasM.draw(); 
+// merge.create();
+
+//Shellsort
+// Merge sort   myCanvasMerge
+// const canvasShell = new Canvas('myCanvasShell');
+// const shell = new Shellsort("#select6A","#select6B", "Big0Shell");
+// function sortShell(){
+//   canvasShell.sort();
+// }
+// function createShell(){
+//     shell.create();
+//   }
+// canvasShell.addElement(shell);
+// canvasShell.draw(); 
+// shell.create();
