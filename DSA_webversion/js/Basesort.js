8123fcfb7220ba728efacc5367defce688313313
phamvutuyetanh <phamvutuyetanh@gmail.com>
2024-01-07 10:42:04 +0700

    update

--- DSA_webversion/js/Basesort.js
+++ DSA_webversion/js/Basesort.js
@@ -20,16 +20,13 @@ class Basesort {
 
   
 
-    create(){
-            
+    create(){      
         this.cols = [];
         this.array = []; 
         var selectElement = document.querySelector(this.namebutton1);
         this.nElems = selectElement.value;
-
         var selectspeed = document.querySelector(this.namebutton2);
          this.speed = selectspeed.value;
-
         if(this.nElems == 10){
             this.array = [7,21,30,39,48,57,66,72,81,87];
         }
@@ -41,21 +38,15 @@ class Basesort {
         }
         this.array = this.shuffle(this.array);
         this.cop = [...this.array];
-        var spacing = (180 - this.magrin*2)/this.nElems;
+        const spacing = (180 - this.magrin*2)/this.nElems;
         this.moves=[];
         for(let i=0; i<this.array.length; i++){
             const x = (i*spacing+spacing/2 + this.magrin)*4;
             const y = this.mycanvasheight - (this.magrin*2) - i *7;
-            var width1 = 0;
-            if(this.nElems == 10){
-                 width1 = (spacing-5)*4.5;
-            }
-            else if (this.nElems == 20){
-                width1 = (spacing-4)*4.5;
-            }
-           else{
-                width1 = (spacing- 3)*4.5;
-           }
+            let width1 = 0;
+            if(this.nElems == 10){ width1 = (spacing-5)*4.5;}
+            else if (this.nElems == 20){ width1 = (spacing-4)*4.5;}
+            else{ width1 = (spacing- 3)*4.5;}
             const width = width1;
             const height = this.maxheight*this.array[i];
             this.cols[i] = new Column(x, y, width, height,this.speed);     
@@ -64,14 +55,12 @@ class Basesort {
     createagain(){
         this.cols = [];
         this.array = [...this.cop];
-        var selectspeed = document.querySelector(this.namebutton2);
-         this.speed = selectspeed.value;
-        var spacing = (180 - this.magrin*2)/this.nElems;
+        const spacing = (180 - this.magrin*2)/this.nElems;
         this.moves=[];
         for(let i=0; i<this.array.length; i++){
             const x = (i*spacing+spacing/2 + this.magrin)*4;
             const y = this.mycanvasheight - (this.magrin*2) - i *7;
-            var width1 = 0;
+            let width1 = 0;
             if(this.nElems == 10){
                  width1 = (spacing-5)*4.5;
             }
@@ -132,18 +121,22 @@ class Basesort {
     }
 
     sort(){
-    var selectElement = document.querySelector(this.namebutton1);
-    var selectvalue = selectElement.value;
+    let selectElement = document.querySelector(this.namebutton1);
+    let selectvalue = selectElement.value;
+    let selectspeed = document.querySelector(this.namebutton2);
+    let speedvalue = selectspeed.value;
     if(selectvalue != this.nElems){
         this.create();
     }
+    else if(speedvalue != this.speed){
+        this.speed = speedvalue;
+    }   
         this.createagain();
         this.moves=this.sortt(this.array);
         
     }
 
     sortt(array){
-    console.log("HE");
       
     }
 
